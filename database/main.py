01df2f513e299b5ba5c567c716c2c2c0b9b83ece
Akmal Rasyid <180747739+Livietan@users.noreply.github.com>
2026-08-28 09:08:33 +0700

    Add files via upload

---
+++ database/main.py
@@ -0,0 +1,44 @@
+import sqlite3
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins={"*"},
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/ADD")
+def add(name:str, type:str, category:str, amount:int, price:int):
+    connect = sqlite3.connect("inventory.db")
+    cursor = connect.cursor()
+
+    cursor.execute(f"""
+    INSERT INTO items (
+    name,
+    type,
+    category,
+    amount,
+    price
+    ) VALUES (?,?,?,?,?)
+    """, (name, type, category, amount, price))
+
+    connect.commit()
+    connect.close()
+
+
+@app.get("/show")
+def main():
+    connect = sqlite3.connect("inventory.db")
+    cursor = connect.cursor()
+
+    cursor.execute("SELECT id, name, type, amount, category , price FROM items")
+
+    data = cursor.fetchall()
+
+    return data
+
+
